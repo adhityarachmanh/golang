7948372348606e7e4f8c4688f224849c6908d1a1
Adhitya Rachman H <adhityarachmanh@gmail.com>
2020-10-15 08:32:40 +0700

    make it better

--- pkg/utils/ed.go
+++ pkg/utils/ed.go
@@ -13,6 +13,71 @@ import (
 	"strings"
 )
 
+type BNESchema struct {
+	Genkey func(int) string
+	_k     string
+	_bL    int
+	Enc    func(string) string
+	Dec    func(string) string
+}
+
+func (self *BNESchema) BNE(bL int, s int) *BNESchema {
+	k := EdGenkey(s)
+	if k == "" {
+		self._k = EdGenkey(-1)
+	} else {
+		self._k = k
+	}
+	self._bL = bL
+	// untuk ENC
+
+	self.Enc = func(s string) string {
+		d := 0
+		l := 0
+		m := 8
+		b := Rank(2, self._bL) - 1
+		r := ""
+		j := len(s)
+		i := 0
+		for i < j {
+			d = (d << m) + int(s[i])
+			i += 1
+			l += m
+			for l >= self._bL {
+				l -= self._bL
+				r += string(self._k[(d>>l)&b])
+				d &= Rank(2, l) - 1
+			}
+		}
+		if l > 0 {
+			r += string(self._k[(d<<(self._bL-l))&b])
+		}
+		return r
+	}
+
+	self.Dec = func(s string) string {
+		d := 0
+		l := 0
+		m := 8
+		r := ""
+		j := len(s)
+		i := 0
+		for i < j {
+			d = ((d & 255) << self._bL) + strings.Index(self._k, string(s[i]))
+			i += 1
+			l += self._bL
+			if l >= m {
+				l -= m
+				r += string(rune((d >> l) & 255))
+			}
+		}
+		return r
+	}
+
+	//untuk dec
+	return self
+}
+
 func EdGenkey(tx int) string {
 	var t, p, q, l, x, y int
 	var k, s, c string

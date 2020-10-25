72b10bee09aeab3598d7e6bdcd6629def642060f
Adhitya Rachman H <adhityarachmanh@gmail.com>
2020-10-25 18:26:41 +0700

    test

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -4,11 +4,12 @@ import (
 	"github.com/joho/godotenv"
 	// "log"
 	"os"
+	"strings"
 )
 
 var Version string = "v1.0"
 
-var CREATOR, PRODUCT_ID, PRODUCT, CREATOR_NAME = GetEnvVal()
+var CREATOR, PRODUCT_ID, PRODUCT, CREATOR_NAME, ALLOW_HOST = GetEnvVal()
 var MODE = os.Getenv("MODE")
 var allowOrigin []string = []string{"http://localhost:4200"}
 var allowMethods []string = []string{"POST", "OPTIONS", "GET", "PUT", "DELETE"}
@@ -31,18 +32,21 @@ var allowheaders []string = []string{"Content-Type", "Authorization"}
 // }
 var Debug bool = true
 
-func GetEnvVal() (string, string, string, string) {
+func GetEnvVal() (string, string, string, string, []string) {
 	godotenv.Load()
+	var ahh []string
 	c := os.Getenv("CREATOR")
 	pi := os.Getenv("PRODUCT_ID")
 	p := os.Getenv("PRODUCT")
 	cn := os.Getenv("CREATOR_NAME")
-	return c, pi, p, cn
+	ah := strings.TrimSpace(os.Getenv("ALLOW_HOST"))
+	ahh = strings.Split(ah, ",")
+	return c, pi, p, cn, ahh
 }
 
 func GetCorsConfig() ([]string, []string, []string, bool) {
 	if MODE == "PROD" {
-		allowOrigin = []string{"https://cv-arh.web.app"}
+		allowOrigin = ALLOW_HOST
 		allowMethods = []string{"POST", "GET"}
 		Debug = false
 	}

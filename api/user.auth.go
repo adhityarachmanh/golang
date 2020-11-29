ba56b1906862b10087db547ded1ada657d70261a
Adhitya Rachman H <adhityarachmanh@gmail.com>
2020-11-29 18:11:28 +0700

    test

--- api/user.auth.go
+++ api/user.auth.go
@@ -37,7 +37,7 @@ func (app *AppSchema) user_auth_notif(c *gin.Context) {
 					Notification: models.NotificationSchema{
 						Title:       visitor.Name,
 						Body:        fmcRequest.Message,
-						ClickAction: "ARH_NOTIF_CLICK",
+						ClickAction: "FLUTTER_NOTIFICATION_CLICK",
 					},
 					Data: visitor,
 					To:   admin.NotificationToken,

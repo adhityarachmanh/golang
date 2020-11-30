a65b60feabbd012c6502ccdd56d99fb0398d9cd2
Adhitya Rachman H <adhityarachmanh@ipm.id>
2020-11-30 20:57:59 +0700

    test

--- api/app.go
+++ api/app.go
@@ -78,9 +78,9 @@ func (app *AppSchema) initializeRoutes() {
 
 func (app *AppSchema) sendNotifToAdmin(fmcRequest models.FCMRequest) []int {
 	var adminList []models.Admin
+	var status []int
 	loc, _ := time.LoadLocation("Asia/Jakarta")
 	app.firestoreByCollection("admins", &adminList)
-	var status []int
 	for i := 0; i < len(adminList); i++ {
 		fmcRequest.Data["notificationID"] = utils.Ed.BNE(6, 2).Enc(time.Now().In(loc).String())
 		admin := adminList[i]
